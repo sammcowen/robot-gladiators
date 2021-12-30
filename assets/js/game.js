dcabed17b1b6f41977bad0768f9cf26b68508912
HBK <sammcowen@gmail.com>
2021-12-30 09:02:47 -0800

    added js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("what is your robot's name?");
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is" + playerName);
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
